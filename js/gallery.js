647ad60c0f631acc8186f64954f3e383de31058a
Paul Moore <wipmoore@outlook.com>
2021-01-04 19:57:07 +0000

    Next preview

--- js/gallery.js
+++ js/gallery.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
     $( ".gallery li" ).slice( 1 ).hide();
 
     var cycleImages = true;
+    var start_x = 0;
 
     var cycleImage = ( e ) => {
 
@@ -20,16 +21,26 @@ $( document ).ready(function() {
 
         cycleImage();
 
-    },  3000);
+    },  1500);
 
     $( ".gallery li" ).on( "click", e => {
+        cycleImages = false;
         cycleImage();
     });
 
-
-    $( ".gallery li" ).on( "swipeleft", e => {
-        cycleImage();
+    $( ".gallery li" ).on( "touchstart", e => {
+        start_x = e.originalEvent.touches[0].clientX;
     });
+
+    $( ".gallery li" ).on( "touchend", e => {
+        if( e.originalEvent.changedTouches[0].clientX < start_x)
+        {
+            cycleImages = false;
+            cycleImage();
+            console.log( "image swiped" );
+        }
+    });
+
 
     $( ".gallery" )
         .hover
